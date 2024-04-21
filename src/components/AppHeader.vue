f8b409cd6af978b731f498097fec318cad659b7b
Jakub Rumpel <jakub@rumpel.dev>
2024-04-22 00:31:40 +0200

    Added main UI components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="border-b-2 rounded-3xl w-auto h-auto bg-gray-100 p-1">
+    <a href="/public" class="size-8 font-bold ml-3">URDF Generator</a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app-header'
+}
+</script>
---
+++ src/components/robot-parts.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-gray-100 rounded-3xl m-auto block w-[65%] h-[90vh]">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
