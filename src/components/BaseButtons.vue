7204494356fa9987225bcfd8cdc69863719babb5
Jakub Rumpel <jakub@rumpel.dev>
2024-05-01 00:37:13 +0200

    Fixed ids for xml buttons

--- src/components/BaseButtons.vue
+++ src/components/BaseButtons.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div class="rounded-3xl mx-auto mt-10 h-56 block w-[30%] bg-gray-200">
-    <button id="removeAll" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-20 w-80 mx-7 mt-7 rounded-xl">Download XML file</button>
-    <button id="removeAll" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-20 w-80 mx-7 mt-7 rounded-xl">Copy XML</button>
-    <button id="removeAll" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 w-80 mx-7 mt-7 rounded-xl">Remove all Elements</button>
+    <button id="downloadXml" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-20 w-80 mx-7 mt-7 rounded-xl">Download XML file</button>
+    <button id="copyXml" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-20 w-80 mx-7 mt-7 rounded-xl">Copy XML</button>
+    <button id="removeAll" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 w-80 mx-7 mt-7 rounded-xl" @click="$emit('removeAll')">Remove all Elements</button>
   </div>
 </template>
 
