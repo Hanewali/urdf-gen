522c84b17afb67296f4624b81a6897e37d0ebacb
Jakub Rumpel <jakub@rumpel.dev>
2024-05-01 00:37:30 +0200

    Added logic for removeAll button

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,26 @@
 import RobotParts from '@/components/RobotParts.vue'
 import Buttons from '@/components/BaseButtons.vue'
 import AppHeader from '@/components/AppHeader.vue'
+import { ref } from 'vue'
+
+const deleteParts = ref(false)
+
+function removeAll() {
+  deleteParts.value = true
+}
+
+function removed(){
+  deleteParts.value = false
+}
+
 </script>
 
 <template>
   <app-header />
 
   <main class="flex pt-3">
-    <robot-parts/>
-    <buttons/>
+    <robot-parts :delete-parts="deleteParts" @removed="removed"/>
+    <buttons @removeAll="removeAll"/>
   </main>
 </template>
 
